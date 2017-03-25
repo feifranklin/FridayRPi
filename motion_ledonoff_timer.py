48fdc18b8eebf616e601b0425ecf2862bc6c70cf
Frank Fei <feifrank@yahoo.com>
2017-03-24 17:48:51 -0700

    use globalVar to control callback

--- motion_ledonoff_timer.py
+++ motion_ledonoff_timer.py
@@ -9,19 +9,25 @@ MotionPin=11
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(LCDPin, GPIO.OUT)
 GPIO.setup(MotionPin, GPIO.IN)
+myVar = 0
 
 def MotionCallback(pin):
+    global myVar
     f = open('motion.log', 'a')
     if GPIO.input(pin):
         print "rising"
         GPIO.output(LCDPin, True)
         f.write(time.strftime("motion detected: %a, %d %b %Y %H:%M:%S +0000\n", time.localtime()))
+        f.close()
+        if myVar < 5:
+            myVar += 1
+        else:
+            print 'prepare email'
+            motion_email.MotionSendEmail()
+            myVar = 0  
     else:
         print "falling"
         GPIO.output(LCDPin, False)
-    f.close()
-    print 'prepare email'
-    motion_email.MotionSendEmail()
 
 
 GPIO.add_event_detect(MotionPin, GPIO.BOTH, callback=MotionCallback)
